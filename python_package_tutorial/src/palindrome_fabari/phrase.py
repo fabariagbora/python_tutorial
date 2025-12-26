b17c2857c8f667dbe893b1bd2a3e0848a944965b
Fabari <agfabariagbora@gmail.com>
2025-12-26 20:51:05 +0100

    Refactor code structure for improved readability and maintainability
    Added palindrome checker for digits and changed the version from 0.0.12 to 0.1.0

--- python_package_tutorial/src/palindrome_fabari/phrase.py
+++ python_package_tutorial/src/palindrome_fabari/phrase.py
@@ -2,21 +2,21 @@ import re
 class Phrase:
     """A class to represent phrases."""
     def __init__(self, content):
-        self.content = content
+        self.content = str(content)
     def processed_content(self):
         """Process the content for palindrome testing."""
-        return self.letters().lower()
+        return self.letters_and_digits().lower()
     def ispalindrome(self):
         """Return True for a palindrome, False otherwise."""
         return self.processed_content() == reverse(self.processed_content())
      
     def letters(self):
         """Return only the letters in the content."""
-        the_letters = []
-        for character in self.content:
-            if re.search("[a-zA-Z]", character):
-                the_letters.append(character)
-        return "".join(the_letters)
+        return "".join(re.findall(r"[A-Za-z]", self.content))
+    
+    def letters_and_digits(self):
+        """Return only the letters and digits in the content."""
+        return "".join(re.findall(r"[A-Za-z0-9]", self.content))
 
 def reverse(string):
     """Reverse a string."""
