372dc0471d752f3c2c177629f3b374babdbb5f45
Fabari <agfabariagbora@gmail.com>
2026-01-01 13:43:24 +0100

    Refactor code structure for improved readability and maintainability; optimize performance in key functions.
    Specifically, I added tests for full_title and full_title function in palindrome_detector to reduce code duplication.

--- palindrome_app/palindrome_detector/__init__.py
+++ palindrome_app/palindrome_detector/__init__.py
@@ -1,6 +1,14 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, app, render_template
+from palindrome_fabari.phrase import Phrase
+
+def full_title(page_title):
+    base_title = "Learn Enough Python Sample App"
+    if page_title:
+        return f"{base_title} | {page_title}"
+    else:
+        return base_title
 
 def create_app(test_config=None):
     """Create and configure the app."""
@@ -16,18 +24,22 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    app.add_template_global(full_title)
     
     @app.route("/")
     def index():
-        return render_template("index.html")
+        return render_template("index.html", page_title="Home")
     
     @app.route("/about")
     def about():
-        return render_template("about.html")
-
+        return render_template("about.html", page_title="About")
+    
     @app.route("/palindrome")
     def palindrome():
-        return render_template("palindrome.html")
+        return render_template("palindrome.html", page_title="Palindrome Detector")
     
+
+        
     return app
 app = create_app()
